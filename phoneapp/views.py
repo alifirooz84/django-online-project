f873d7ae34b659b08f97a66bba742a13353f682f
alifirooz84 <jhma373@gmail.com>
2025-08-04 17:01:32 +0330

    Update views.py

--- phoneapp/views.py
+++ phoneapp/views.py
@@ -1,1 +1,13 @@
+from django.shortcuts import render, redirect
+from .forms import PhoneNumberForm
+
+def phone_input(request):
+    if request.method == 'POST':
+        form = PhoneNumberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    else:
+        form = PhoneNumberForm()
+    return render(request, 'phone_input.html', {'form': form})
 
